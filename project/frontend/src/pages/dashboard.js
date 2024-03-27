b588a9bfb43091d0451da6ed6cbf90d9d3f482b8
Chokchai213 <chokchai.bis@gmail.com>
2024-03-27 11:28:49 +0700

    fix another bug

--- project/frontend/src/pages/dashboard.js
+++ project/frontend/src/pages/dashboard.js
@@ -9,110 +9,113 @@ import axios from "axios";
 const baseURL = "http://localhost:8000";
 
 function fetchUserData(userStore) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`${baseURL}/db/userdata_dashboard`, {
-        headers: {
-          Authorization: userStore.userToken,
-          userId: userStore.userId,
-        },
-      })
-      .then((response) => {
-        let data = [];
-        response.data.queryResult.forEach((e) => {
-          data.push(e);
-        });
-        resolve(data);
-      })
-      .catch((error) => {
-        console.log("err :: ", error);
-        reject(error);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${baseURL}/db/userdata_dashboard`, {
+                headers: {
+                    Authorization: userStore.userToken,
+                    userId: userStore.userId,
+                },
+            })
+            .then((response) => {
+                let data = [];
+                response.data.queryResult.forEach((e) => {
+                    data.push(e);
+                });
+                resolve(data);
+            })
+            .catch((error) => {
+                console.log("err :: ", error);
+                reject(error);
+            });
+    });
 }
 
 const groupDataByYear = (data) => {
-  return data.reduce((acc, item) => {
-    const year = item.date.split("-")[0];
-    const existingYear = acc.find((entry) => entry.year === year);
-    if (existingYear) {
-      existingYear.data.push(item);
-    } else {
-      acc.push({ year, data: [item] });
-    }
-    return acc;
-  }, []);
+    return data.reduce((acc, item) => {
+        const year = item.date.split("-")[0];
+        const existingYear = acc.find((entry) => entry.year === year);
+        if (existingYear) {
+            existingYear.data.push(item);
+        } else {
+            acc.push({ year, data: [item] });
+        }
+        return acc;
+    }, []);
 };
 
 export const Dashboard = () => {
-  const userStore = useSelector((state) => state.userStore);
-  const dispatch = useDispatch();
-  const [userData, setUserData] = useState(null);
-  useEffect(() => {
-    Promise.all([fetchUserData(userStore)]).then((res) => {
-      const data = res[0];
-      if (data.length > 0) {
-        const groupedData = groupDataByYear(data);
-        const yearInData = groupedData.map((data) => data.year);
-        if (yearInData.length > 0) {
-          let dataYear = groupedData.find(
-            (entry) => entry.year === yearInData[0]
-          );
-          let tmpMonthArray = dataYear.data;
-          tmpMonthArray.sort((a, b) => {
-            if (a.date < b.date) {
-              return -1;
+    const userStore = useSelector((state) => state.userStore);
+    const dispatch = useDispatch();
+    const [userData, setUserData] = useState(null);
+    useEffect(() => {
+        Promise.all([fetchUserData(userStore)]).then(res => {
+            const data = res[0];
+            if (data.length > 0) {
+                const groupedData = groupDataByYear(data);
+                const yearInData = groupedData.map((data) => data.year);
+                if (yearInData.length > 0) {
+                    let dataYear = groupedData.find((entry) => entry.year === yearInData[0]);
+                    let tmpMonthArray = dataYear.data;
+                    tmpMonthArray.sort((a, b) => {
+                        if (a.date < b.date) {
+                            return -1;
+                        }
+                        if (a.date > b.date) {
+                            return 1;
+                        }
+                        return 0;
+                    });
+                    dataYear.data = tmpMonthArray;
+                }
+                setUserData(groupedData);
+            } else {
+                const currentDate = new Date();
+                const currentYearObj = {
+                    data: [],
+                    year: currentDate.getFullYear().toString(),
+                };
+                setUserData([currentYearObj]);
             }
-            if (a.date > b.date) {
-              return 1;
-            }
-            return 0;
-          });
-          dataYear.data = tmpMonthArray;
-        }
-        setUserData(groupedData);
-      } else {
-        const currentDate = new Date();
-        const currentYearObj = {
-          data: [],
-          year: currentDate.getFullYear().toString(),
-        };
-        setUserData([currentYearObj]);
-      }
-    });
-  }, [userStore]);
+        })
+    }, [userStore])
 
-  try {
-    if (userStore.userId === null) {
-      const localUser = localStorage.getItem("userData");
-      if (localUser) {
-        axios
-          .post(`${baseURL}/auth/veriylocaluser`, {
-            localUser: localUser,
-          })
-          .then((res) => {
-            dispatch(LoginWithLocalData(JSON.parse(localUser)));
-          })
-          .catch((e) => {
-            localStorage.removeItem("userData");
-            console.log(e);
-          });
-      }
+    try {
+        if (userStore.userId === null) {
+            const localUser = localStorage.getItem("userData");
+            if (localUser) {
+                axios
+                    .post(`${baseURL}/auth/veriylocaluser`, {
+                        localUser: localUser,
+                    })
+                    .then((res) => {
+                        dispatch(LoginWithLocalData(JSON.parse(localUser)));
+                    })
+                    .catch((e) => {
+                        localStorage.removeItem("userData");
+                        console.log(e);
+                    });
+            }
+        }
+    } catch (e) {
+        console.log(e);
     }
-  } catch (e) {
-    console.log(e);
-  }
-  return (
-    <React.Fragment>
-      <Navigate />
-      <Grid container marginTop={2} justifyContent={"center"}>
-        <Grid>
-          <PieChartComponent userData={userData} />
-        </Grid>
-        <Grid>
-          <MonthDataTable userData={userData} setUserData={setUserData} />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Navigate />
+            <Grid container marginTop={2} justifyContent={"center"}>
+                <Grid>
+                    <PieChartComponent
+                        userData={userData}
+                    />
+                </Grid>
+                <Grid>
+                    <MonthDataTable
+                        userData={userData}
+                        setUserData={setUserData}
+                    />
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
 };
--- project/frontend/src/pages/goalbased.js
+++ project/frontend/src/pages/goalbased.js
@@ -17,18 +17,17 @@ import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 import { styled } from "@mui/material/styles";
 import { AssetSummary } from "components/AssetSummary_GoalBased";
 import CircularProgress from "@mui/joy/CircularProgress";
-import { set } from "store";
 
-const warnTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-        backgroundColor: theme.palette.common.white,
-        color: "rgba(0, 0, 0, 0.87)",
-        boxShadow: theme.shadows[1],
-        fontSize: 11,
-    },
-}));
+// const warnTooltip = styled(({ className, ...props }) => (
+//     <Tooltip {...props} classes={{ popper: className }} />
+// ))(({ theme }) => ({
+//     [`& .${tooltipClasses.tooltip}`]: {
+//         backgroundColor: theme.palette.common.white,
+//         color: "rgba(0, 0, 0, 0.87)",
+//         boxShadow: theme.shadows[1],
+//         fontSize: 11,
+//     },
+// }));
 
 export const GoalBased = () => {
     const navigate = useNavigate();
